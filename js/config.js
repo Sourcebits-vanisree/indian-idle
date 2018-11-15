5e892c2e13fa2eec6429863e1d3988bc4d72719a
Nijamudheen <Nijamudheen@SBMAC0307.local>
2018-11-15 20:31:30 +0530

    timer

---
+++ js/config.js
@@ -0,0 +1,2 @@
+
+var api_url = JSON.parse('{"voteForContestant" :"https://qa-emsapi.sonyliv.com/api/v1/vote/voteForContestant","votePerUserForContestants" :"https://qa-emssdk.sonyliv.com/api/v1/admin/votePerUserForContestants","top_Ranks" :"https://s3.ap-south-1.amazonaws.com/qa-ems-app-data/leaderboard/prog-3/topranking/leader.json","get_Profile" :"https://s3.ap-south-1.amazonaws.com/qa-ems-app-data/leaderboard/prog-3/profiledetails/ProfileDetails"}');
